79f814b9cfee829790b4a09c6680ae323ef92b2b
kwonhwan <juoo2@nate.com>
2019-06-01 01:21:48 +0900

    이메일정보 가져와서 mypage textview에 저장

--- app/src/main/java/com/hufs/cdt/Mypage.java
+++ app/src/main/java/com/hufs/cdt/Mypage.java
@@ -6,12 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class Mypage extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage);
+        Intent intent= getIntent();
+        String strEmail = intent.getStringExtra("email");
+        TextView textView=(TextView)findViewById(R.id.login_id);
+        textView.setText(strEmail);
 
 
         Button post_btn1 = (Button)findViewById(R.id.post_btn1);
